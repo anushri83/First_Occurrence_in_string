5e7a10e97b544e32521e5db25d773a9751cf06a3
anushri83 <131172501+anushri83@users.noreply.github.com>
2024-08-09 15:52:58 +0530

    First_Occurrence_in_string

--- First_Occurrence_in_string.py
+++ First_Occurrence_in_string.py
@@ -1,8 +1,12 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        m=len(needle)
-        for i in range(0,m):
-            if haystack[0:m]==needle:
-                return 0
-            else:
-                return -1
+         if not needle:
+             return 0 
+         
+         n,m = len(haystack),len(needle)
+         
+         for i in range(n - m + 1):
+             if haystack[i:i + m] == needle:
+                 return i
+         
+         return -1
